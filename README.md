d3dd7b84927886520b130929e2f4231bd2182601
Pranit <pranit.t.zope@gmail.com>
2023-04-09 23:03:23 +0530

    basic ray and distance detection

--- README.md
+++ README.md
@@ -8,13 +8,33 @@ make
 ```
 
 *Controls*
-* Forward
-* Backward
-* Left 
-* Right
+* `W` - Forward
+* `S` - Backward
+* `A` - Left 
+* `D` - Right
 
 ## Approach for Making
 
-Firstly, `std::cout` isn't going to work since the time taken is much higher than directly writing on the console. Hence we will use conventional `printf` and its variants for our main display.
+Firstly, `std::cout` isn't going to work since the time taken is much higher than directly writing on the console. Hence we will use conventional `printf` or similar methods and its variants for our main display.
+
+### Made a screen buffer of size `120x40`
+Using `windows.h` created a screen buffer using `createConsoleScreenBuffer()` and service to print directly onto it any desired manner using the coordinates using the `WriteConsoleOutputCharacter()` method.
+
+### Made a basic Map
+* Made a 16X16 map to move around and demonstrate the basic ray tracing
+* `#` represent the wall
+* `.` represent the empty space
+<!-- * `P` represent the player position -->
+
+### Initialised the main Game Loop
+Just simply using `while(true)` for now for the continuous rendering.
+
+## Algorithm
+The following image contains the player, the wall and the FOV of the player in the game (red dotted line).
+
+![Depiction of the algorithm](docs/potters_algo.png)
+
+We calculate the distance to the wall by incrementing until the flag `bHitWall` becomes true. (Depicted by the blue dashed line).
+
 
 
--- ray_tracing.cpp
+++ ray_tracing.cpp
@@ -7,19 +7,28 @@
 #include <math.h>
 #include <Windows.h>
 
-int nScreenWidth = 120;			// Console Screen Size X (columns)
-int nScreenHeight = 40;			// Console Screen Size Y (rows)
-int nMapWidth = 16;				// World Dimensions W
-int nMapHeight = 16;            // World Dimensions H
+int screenWidth = 120;			// Console Screen Size X (columns)
+int screenHeight = 40;			// Console Screen Size Y (rows)
+int mapWidth = 16;				// World Dimensions W
+int mapHeight = 16;             // World Dimensions H
 
-float fPlayerX = 14.7f;		    // Player Start Position X
-float fPlayerY = 5.09f;         // Player Start Position Y
-float fPlayerA = 0.0f;			// Player Start Rotation
+float playerX = 8.0f;		    // Player Start Position X
+float playerY = 8.0f;           // Player Start Position Y
+float playerA = 0.0f;			// Player Start Rotation
+// float playerA = M_PI_4;			
+// float playerA = M_PI_2;			
+// float playerA = 3*M_PI_4;			
  
-float fFOV = M_PI_4;	        // Field of View (pi/4) 
+float FOV = M_PI_4;	            // Field of View (pi/4) 
 
-float fDepth = 16.0f;			// Maximum rendering distance
-float fSpeed = 5.0f;
+float depth = 16.0f;			// Maximum rendering distance
+// float speed = 5.0f;             // Movement speed
+
+typedef struct uvec {
+    float X;
+    float Y;
+    //float Z;  //not needed for now
+} uvec;
 
 
 int main (int argc, char **argv){
@@ -27,44 +36,82 @@ int main (int argc, char **argv){
     /**
      * Creating a screen buffer
      * Contains lot of things specific to windows, hence seems a bit complicated
-     * used windows.h to create a buffer of size nScreenWidth X nScreenHeight
+     * used windows.h to create a buffer of size screenWidth X screenHeight
     */
-    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,__null,CONSOLE_TEXTMODE_BUFFER,__null);
-    SetConsoleActiveScreenBuffer(hConsole);
+    wchar_t *screen = new wchar_t[screenWidth*screenHeight];
+    HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,__null,CONSOLE_TEXTMODE_BUFFER,__null);
+    SetConsoleActiveScreenBuffer(console);
     DWORD dwBytesWritten = 0;
 
     //Creating the map
     std::wstring map;
 
-    map+= L"###############";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"#.............#";
-    map+= L"###############";
+    map+= L"################";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"#..............#";
+    map+= L"################";
 
 
 
     // Main loop
     while(true){
-        //main content goes here
+        
+        // We will deal column wise hence a for loop for column operations
+        for(int x=0; x< screenWidth; x++){
 
+            //calculate the ray angle, and find the distance to the wall using a flag, and computing the eye angles
+            float rayAngle = (playerA - FOV/2.0f) + ((float)x / (float)screenWidth) * FOV;
+            float distanceToWall = 0; 
+            bool hitWall = false;      
+
+            //unit vector for ray
+            uvec eye;
+            eye.X = sinf(rayAngle);
+            eye.Y = cosf(rayAngle);
+
+            while (!hitWall && distanceToWall < depth){
+                distanceToWall += 0.1f;  //small increment for more precision
+
+                int testX = (int)(playerX + ((eye.X)*distanceToWall));
+                int testY = (int)(playerX + ((eye.Y)*distanceToWall));
+
+                // check the position wrt boundaries of map 
+                if(testX<0 || testX>=mapWidth || testY<0 || testY>=mapHeight){
+                    hitWall = true;
+                    distanceToWall = depth;
+                }
+                else if( map[(testY*mapWidth) + testX] == '#'){
+                    hitWall = true;
+                    // distanceToWall value is retained (increment one)
+                }
+
+            }
+
+            //now for ceiling, we can use simple geometry and trim portions form up and down symmetrically
+            int ceiling = (float)(screenHeight/2.0) - screenHeight/((float)distanceToWall);
+            int floor = screenHeight - ceiling;
+
+            for(int y=0; y<screenHeight; y++){
+                screen[y*screenWidth + x] = ((y<=ceiling)?' ':((y>ceiling&&y<=floor)?'#':' '));
+            }
+
+        }
 
         //Display Frame
-        screen[nScreenWidth*nScreenHeight-1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0,0}, &dwBytesWritten);
+        screen[screenWidth*screenHeight-1] = '\0';
+		WriteConsoleOutputCharacterW(console, screen, screenWidth*screenHeight, {0,0}, &dwBytesWritten);
     }
 
     return 0;
